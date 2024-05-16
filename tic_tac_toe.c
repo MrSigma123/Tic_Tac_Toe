4fefb440bd6fc0faed63fa3081898636929764b5
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-16 23:50:12 +0200

    Add project design overview

---
+++ tic_tac_toe.c
@@ -0,0 +1,11 @@
+/* tic_tac_toe.c -- console version of Tic Tac Toe game */
+/* 
+  Project design:
+  
+  TODO:
+  - generate the gaming grid
+  - implement engine for CPU
+  - implement two players mode
+    let the user to decide to play X or O (or choose randomly)
+  - add counting the score (wins by X and O)
+*/
