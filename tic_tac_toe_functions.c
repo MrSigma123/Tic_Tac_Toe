90a4f99c2a528966d98792edb1620afa4797e729
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-27 23:24:22 +0200

    Prepare tic_tac_toe_functions.c definitions

---
+++ tic_tac_toe_functions.c
@@ -0,0 +1,40 @@
+/* tic_tac_toe_functions.c */
+
+#include <stdio.h>
+#include "tic_tac_toe_functions.h"
+
+void logo(void) {
+  
+}
+
+void welcome_message(void) {
+
+}
+
+void menu(void) {
+
+}
+
+void display_grid(void) {
+
+}
+
+void two_player_mode(void) {
+
+}
+
+void easy_mode(void) {
+
+}
+
+void medium_mode(void) {
+
+}
+
+void hard_mode(void) {
+
+}
+
+int choose_X_or_O(void) {
+  return 0;
+}
