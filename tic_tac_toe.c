5689f02f9dce6d44fc4f2945b74bea1447c99f49
MrSigma123 <karol.czolpinski@gmail.com>
2025-01-05 22:45:14 +0100

    Add Tic Tac Toe main file

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -13,6 +13,35 @@
 #include <stdio.h>
 #include "tic_tac_toe_functions.h"
 
-int main(void) {
-  return 0;
+int main() {
+    char board[3][3];
+    int player = 1;
+    int winner = 0;
+    int move_count = 0;
+
+    initialize_board(board);
+
+    while (winner == 0 && move_count < 9) {
+        display_board(board);
+        printf("Player %d's turn. Enter row and column (0-2): ", player);
+        int row, col;
+        scanf("%d %d", &row, &col);
+
+        if (make_move(board, row, col, player)) {
+            move_count++;
+            winner = check_winner(board);
+            player = (player == 1) ? 2 : 1;
+        } else {
+            printf("Invalid move. Try again.\n");
+        }
+    }
+
+    display_board(board);
+    if (winner) {
+        printf("Player %d wins!\n", winner);
+    } else {
+        printf("It's a draw!\n");
+    }
+
+    return 0;
 }
