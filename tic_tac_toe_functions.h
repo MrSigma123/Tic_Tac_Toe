c1b8285c2f3061e505a0e26e2492aa2c3e9d48e2
MrSigma123 <karol.czolpinski@gmail.com>
2025-01-05 22:47:16 +0100

    Add Tic Tac Toe header file

--- tic_tac_toe_functions.h
+++ tic_tac_toe_functions.h
@@ -1,10 +1,9 @@
-/* tic_tac_toe_functions.h */
-void logo(void);
-void welcome_message(void);
-void menu(void);
-void display_grid(void);
-void two_player_mode(void);
-void easy_mode(void);         /* random placement as CPU        */
-void medium_mode(void);       /* simple rules for CPU strategy  */
-void hard_mode(void);         /* sophisticated CPU algorithms   */
-int choose_X_or_O(void);
+#ifndef TIC_TAC_TOE_FUNCTIONS_H
+#define TIC_TAC_TOE_FUNCTIONS_H
+
+void initialize_board(char board[3][3]);
+void display_board(char board[3][3]);
+int make_move(char board[3][3], int row, int col, int player);
+int check_winner(char board[3][3]);
+
+#endif // TIC_TAC_TOE_FUNCTIONS_H
